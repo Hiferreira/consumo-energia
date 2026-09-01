6a894566ec77ad86d8619de96589fcff291df24e
Rayane Maria da Silva Ferreira <255997719+Hiferreira@users.noreply.github.com>
2026-09-01 01:46:43 -0300

    Refactor input prompts for clarity and consistency

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-#Entrada de dados
-nome_aparelho = input("Qual o nome do aparelho? ") 
-potencia_w= float(input("Qual a potência do aparelho em Watts(W)?"))
-horas_diarias= float(input("Qual o tempo médio em horas por dia?"))
+# Calculadora de Consumo Elétrico
+# Autor: Rayane Ferreira
+
+# Entrada de dados
+nome_aparelho = input("Digite o nome do aparelho: ") 
+potencia_w= float(input("Digite a potência do aparelho em Watts(W): "))
+horas_diarias= float(input("Digite o tempo médio em horas por dia: "))
 # Processamento de dados
 consumo_mensal_kwh= (potencia_w * horas_diarias * 30)/1000
 custo_estimado= consumo_mensal_kwh * 0.78938
 # Saída de dados
 print(f"Aparelho: {nome_aparelho}")
-print(f"Consumo estimado: {consumo_mensal_kwh} kWh/mês")
+print(f"Consumo estimado: {consumo_mensal_kwh:.2f} kWh/mês")
 print(f"Custo estimado: R$ {custo_estimado:.2f}")
